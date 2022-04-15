5bba639a22f9111155ed07ea14ca20033b0ac270
doffy007 <ochanmimi1@gmail.com>
2022-04-15 14:31:47 +0700

    golang embed

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/doffy007/go-embed.git
+
+go 1.18
---
+++ unit test/embed_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"embed"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"testing"
+)
+
+//go:embed version.txt
+var version string
+
+func TestEmbed(t *testing.T) {
+	fmt.Println(version)
+}
+
+//go:embed logo.png
+var logo []byte
+
+func TestByte(t *testing.T) {
+	err := ioutil.WriteFile("logo_new.png", logo, fs.ModePerm)
+	if err != nil {
+		fmt.Println("cannot embed")
+	}
+}
+
+//go:embed files/a.txt
+//go:embed files/b.txt
+//go:embed files/c.txt
+var files embed.FS
+
+func TestMultipleFile(t *testing.T) {
+
+	a, _ := files.ReadFile("files/a.txt")
+	fmt.Println(string(a))
+
+	b, _ := files.ReadFile("files/b.txt")
+	fmt.Println(string(b))
+
+	c, _ := files.ReadFile("files/c.txt")
+	fmt.Println(string(c))
+}
+
+//go:embed files/*.txt
+var path embed.FS
+
+func TestPathMatcher(t *testing.T)  {
+	dir, _ := path.ReadDir("files")
+	for _, entry := range dir {
+		if !entry.IsDir() {
+			fmt.Println(entry.Name())
+			content, _ := path.ReadFile("files/" + entry.Name())
+			fmt.Println("Content : ", string(content))
+		}
+	}
+}
